26099d5c9cbc53041e657b15c6c57a3b0c6a1844
Jonmikal Vidal <jonmv3code@gmail.com>
2022-10-01 08:06:24 -0600

    Workshop week 1 task 1

---
+++ screens/ContactScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+
+function ContactScreen() {
+  return (
+    <div>
+      <ScrollView></ScrollView>
+    </div>
+  );
+}
+
+export default ContactScreen;
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from "./DirectoryScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./HomeScreen";
+import AboutScreen from "./AboutScreen";
+import ContactScreen from "./ContactScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -49,6 +51,29 @@ const DirectoryNavigator = () => {
     </Stack.Navigator>
   );
 };
+
+const AboutNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="About" component={AboutScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Contact"
+        component={AboutScreen}
+        options={{ title: "Contact Us" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const Main = () => {
   return (
     <View
@@ -70,6 +95,12 @@ const Main = () => {
           name="Directory"
           component={DirectoryNavigator}
           options={{ title: "Directory" }}
+        />
+        <Drawer.Screen name="About" component={AboutNavigator} />
+        <Drawer.Screen
+          name="Contact Us"
+          component={ContactNavigator}
+          options={{ title: "Contact Us" }}
         />
       </Drawer.Navigator>
     </View>
